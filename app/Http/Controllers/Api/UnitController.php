722001b7fbfe1cd6d03000c375671d0bbbdc1dfc
Zhong Weibin <weibin.zhong@maxent-inc.com>
2017-10-28 11:32:36 +0800

    unit controller

---
+++ app/Http/Controllers/Api/UnitController.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: geekzwb
+ * Date: 2017/10/28
+ * Time: 上午11:31
+ */
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Unit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class UnitController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*$units = Unit::with(['purchases' => function ($query) {
+            $query->with(['products' => function ($query1) {
+                $query1->select('products.id', 'products.sku', 'products.name');
+            }]);
+        }])->get();*/
+        $units = Unit::all();
+        return $this->success($units);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'reqired|unique:units',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->failed($validator->errors());
+        }
+
+        $unit = new Unit();
+        $unit->name = $request->get('name');
+        $unit->note = $request->get('note');
+
+        $unit->save();
+
+        return $this->success($unit);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => [
+                'required',
+                Rule::unique('units')->ignore($id),
+            ]
+        ]);
+
+        if ($validator->fails()) {
+            return $this->failed($validator->errors());
+        }
+
+        $unit = Unit::find($id);
+        $unit->name = $request->get('name');
+        $unit->note = $request->get('note');
+
+        $unit->save();
+
+        return $this->success($unit);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $unit = Unit::find($id);
+        $unit->delete();
+
+        $this->message('delete success');
+    }
+}
